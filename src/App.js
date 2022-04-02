ae3133c65c7986def9a4c6ea62f4a4f88233684c
syrgo <syrga.v30@gmail.com>
2022-04-02 20:04:40 +0300

    created destructing assignments

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 
 
 function App() {
+  const example1 = ["apple", "red", "big"];
+  const example2 = ["toyota", "camry", "black", "expensive"];
+
+  const example3 = { name: "apple", color: "red", size: "big" };
+  const example4 = { make: "toyota", modal: "camry", carColor: "black", price: "expensive" };
+
+  // const [name, color, size] = example1;
+  // const [make, modal, carColor, price] = example2;
+
+  const { name, color, size } = example3;
+  const { make, modal, carColor, price } = example4;
+
   return (
     <div className="App">
-      
+      This apple {name} is {color} and {size}. <br />
+      This {make} {modal} is {price} and {carColor}.
     </div>
   );
 }
